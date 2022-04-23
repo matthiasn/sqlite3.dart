0298c4351acdf31b33b8dbb92c397c3b8011d0e1
Simon Binder <oss@simonbinder.eu>
2022-04-23 14:29:29 +0200

    Use prebuilt sqlcipher amalgation

--- sqlcipher_flutter_libs/windows/sqlite3_flutter_libs_plugin.cpp
+++ sqlcipher_flutter_libs/windows/sqlite3_flutter_libs_plugin.cpp
@@ -1,4 +1,4 @@
-#include "include/sqlite3_flutter_libs/sqlite3_flutter_libs_plugin.h"
+#include "include/sqlcipher_flutter_libs/sqlite3_flutter_libs_plugin.h"
 
 void Sqlite3FlutterLibsPluginRegisterWithRegistrar(
     FlutterDesktopPluginRegistrarRef registrar) {
